767f58a93ecddb26d669131c48df27089b6dbb16
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2018-06-04 14:08:19 -0400

    Initial Commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myInt = 5;
+        int myInt2 = 12;
+        int myInt3 = 6;
+
+        int myTotal = myInt + myInt2 + myInt3;
+
+        System.out.println(myTotal);
+    }
+}
